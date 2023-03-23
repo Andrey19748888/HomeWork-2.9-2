e1314829c739d3910cfe8f0526174321cc250398
Andreydenisov7788 <asasa88881974@mail.ru>
2023-03-23 04:40:00 +0300

    дз 2.1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,37 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+
+
+
 public class Main {
     public static void main(String[] args) {
+        Map<String, List<Integer>> map = new HashMap<>();
+        Random r = new Random();
+        for (int i = 0; i < 5; i++) {
+            map.put("str" + i, List.of(r.nextInt(1001),
+                    r.nextInt(1001),
+                    r.nextInt(1001)));
+        }
+
+        System.out.println(map);
+
+        Map<String, Integer> sumMap = new HashMap<>();
+
+        List<Integer> list;
+        int sum;
+        for (String key: map.keySet()) {
+            list = map.get(key);
+            sum = list.get(0) + list.get(1) + list.get(2);
+            sumMap.put(key, sum);
+        }
+
+
+        System.out.println(sumMap);
     }
+
+
+
 }
